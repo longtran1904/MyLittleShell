57614798a6c1645bf451908bc1c07b6871cd6606
Giyuu <tranhoanglong1904@gmail.com>
2023-03-30 00:07:09 -0400

    add pipe - multipipes

--- execution.c
+++ execution.c
@@ -33,21 +33,19 @@ int getCurDir(char* buf){
     return EXIT_SUCCESS;
 };
 
-void execProgram(char*** commands, int len){
-    // Create a child process
-    // Search through all directories for prog_name:
-    // PATH evironments
-
-    char** prog_args = commands[0];
+char* findPath(char* prog_name) {
     bool isPathGiven = false;
-    if (*prog_args[0] == '/') isPathGiven = true;
+    if (prog_name[0] == '/') isPathGiven = true;
 
     // Infos about the filePath, and stat_buf (using stat())
     struct stat stat_buf;
     char* filePath = NULL;
 
-    if (isPathGiven)
-        filePath = prog_args[0]; // take program path from command
+    if (isPathGiven){
+        int pathSize = strlen(prog_name) + 1;
+        filePath = malloc(pathSize);
+        memcpy(filePath, prog_name, pathSize); // take program path from command
+    }
     else
     {
         // look for program based on bare names from command
@@ -66,26 +64,19 @@ void execProgram(char*** commands, int len){
         for (int i = 0; i < path_count; i++)
         {   
             // concatenate PATH[i] and prog_name
-            pathSize = strlen(PATH[i]) + strlen(prog_args[0]) + 1;
+            pathSize = strlen(PATH[i]) + strlen(prog_name) + 1;
             pathSize = ((pathSize + 7)/8)  * 8;
 
             //assign and try paths
             
             tryPath = (char*) malloc(pathSize * sizeof(char));
             strcpy(tryPath, PATH[i]);
-            strcat(tryPath, prog_args[0]); 
+            strcat(tryPath, prog_name); 
 
             if (stat(tryPath, &stat_buf) == 0) // found program
             {
                 if (DEBUG) {
-                    printf(YELLOW "stat() found file %s at dir: %s" RESET "\n", prog_args[0], tryPath);
-                    int i = 0;
-                    printf(YELLOW "args:");
-                    while (prog_args[i] != NULL){
-                        printf("[%s]   ", prog_args[i]);
-                        i++;
-                    }
-                    printf(RESET "\n");
+                    printf(YELLOW "stat() found file %s at dir: %s" RESET "\n", prog_name, tryPath);
                 }
                 filePath = (char*) malloc(pathSize);
                 memcpy(filePath, tryPath, pathSize);
@@ -94,40 +85,108 @@ void execProgram(char*** commands, int len){
             free(tryPath);
         }
     }
+    return filePath;
+}
 
+pid_t execProgram(char*** commands, int* idx, int len, int* pd, int lastPipe){
+    // Create a child process
+    char** prog_args = commands[*idx];
+    int nfd;
+
+    // Implemented for single command so far
+    // TODO: split arguments corresponding each command and execute
+    if (strcmp(*prog_args, "pwd") == 0){
+        char buffer[BUFSIZE];
+        int res = getCurDir(buffer);
+        if (res == 0){  
+            nfd = dup2(pd[1], STDOUT_FILENO);
+            if (nfd == -1) {
+                perror(RED "ERROR: write to pipefailed" RESET "\n");
+            }
+            int writeBytes = write(STDOUT_FILENO, buffer, BUFSIZE);
+            if (writeBytes == -1) printf(RED "Didn't write any byte!!!" RESET "\n");
+            else if (DEBUG) printf(YELLOW "Wrote %d bytes!!" RESET "\n", writeBytes);
+            close(pd[0]);
+            close(pd[1]);
+        }
+        else {
+            printf("pwd failed\n");
+        }
+        return 0;
+    }
+    if (strcmp(*prog_args, "cd") == 0){
+        changeDir(*(*commands+1));
+        return 0;
+    }
+
+    char* filePath = findPath(*prog_args);
+    int pid = 0;
     if (filePath == NULL) {
         // program not found
-        printf("Program %s not found!\n", prog_args[0]);
+        printf("Program %s not found!\n", *prog_args);
     }
     else
     {
         // Run program in filePath
-        int pid = fork();
+
+        pid = fork();
         if (pid == -1) { 
             perror("fork failed");
         }
-        if (pid == 0) {
-            // we are in the child process
-            // Check for redirections:
-            // TODO: Checking valid command for more than 3 commands.
-            if (len > 1) {
-                if (len != 3) printf(RED "ERROR: Invalid command" RESET "\n");
-                
-                else{
-                    if (commands[1][0][0] == '<')
+        if (pid == 0) {// we are in the child process
+            //Get input from pipeline
+            //if not first command.
+            if (*idx != 0 && lastPipe >= 0){
+                if (DEBUG) printf("reading from pipeline[%d]\n", (lastPipe));
+
+                nfd = dup2(pd[lastPipe], STDIN_FILENO);
+                if (nfd == -1){
+                    perror(RED "ERROR: Read from pipeline failed" RESET "\n");
+                }
+            }
+
+            //write output to pipeline
+            //if not last command
+            if (*idx < (len-1))
+            {
+                if (DEBUG) printf("writing to pipeline[%d]\n", (*idx)*2 + 1);
+                nfd = dup2(pd[(*idx)*2 + 1], STDOUT_FILENO);
+                if (nfd == -1){
+                    perror(RED "ERROR: Write to pipeline failed" RESET "\n");
+                }
+            }
+
+            //close all pipes;
+            for (int i = 0; i < 2*len; i++) close(pd[i]);
+
+            // Look for redirection
+            while (len - 1 - *idx > 0 && commands[(*idx+1)][0][0] != '|') {
+                if (commands[*(idx++)][0] != NULL)
+                {
+                    if (commands[*idx][0][0] == '<')
                     {
+                        if (commands[*idx][1] == NULL) {
+                            printf(RED "Invalid Redirection Syntax" RESET "\n");
+                            exit(EXIT_FAILURE);
+                        }
+
                         // Redirect input from a file
-                        int fd = open(commands[2][0], O_RDONLY);
+                        int fd = open(commands[*idx][1], O_RDONLY);
                         int nfd = dup2(fd, STDIN_FILENO);
                         if (nfd == -1) printf( RED "ERROR: Redirect input failed" RESET "\n");
                         close(fd);    
                     }
 
 
-                    if (commands[1][0][0] == '>')
+                    if (commands[*idx][0][0] == '>')
                     {
+                        if (commands[*idx][1] == NULL) {
+                            printf(RED "Invalid Redirection Syntax" RESET "\n");
+                            exit(EXIT_FAILURE);
+                        }
+
                         // Redirect output to a file
-                        int fd = open(commands[2][0], O_WRONLY|O_CREAT|O_TRUNC, 0640);
+                        int fd = open(commands[*idx][1], O_WRONLY|O_CREAT|O_TRUNC, 0640);
                         int nfd = dup2(fd, STDOUT_FILENO);
                         if (nfd == -1) printf( RED "ERROR: Redirect output failed" RESET "\n");
                         close(fd);
@@ -143,42 +202,66 @@ void execProgram(char*** commands, int len){
             // if we reach here, something went wrong
             exit(EXIT_FAILURE);
         }
-        else {
-            int wstatus;
-            int tpid = wait(&wstatus); // wait for child to finish
-            if (tpid == -1) // wait failed
-            {
-                perror("wait failed");
-            }    
-
-            if (WIFEXITED(wstatus) && DEBUG){
-                // child exited normally
-                printf("child exited with %d\n", WEXITSTATUS(wstatus));
-            }
-        }
     }
-    if (!isPathGiven) free(filePath);
+    free(filePath);
+    return pid;
 }
 
 void execute(char ***commands, int len) {
     if (commands == NULL) return;
 
-    // Implemented for single command so far
-    // TODO: split arguments corresponding each command and execute
-	if (strcmp(**commands, "pwd") == 0){
-	    char buffer[BUFSIZE];
-	    int res = getCurDir(buffer);
-	    if (res == 0){  
-		printf("%s\n", buffer);
-	    }
-	    else {
-		printf("pwd failed\n");
-	    }
-	    return;
-	}
-	if (strcmp(**commands, "cd") == 0){
-	    changeDir(*(*commands+1));
-	    return;
-	}
-    execProgram(commands, len);
+    int* pids = malloc(sizeof(int) * 4);
+    int pids_count = 0, pids_size = 4;
+    int command_iterator = 0;
+    int pd[2*len]; 
+    for (int i = 0; i < len;i++){ 
+        if (pipe(pd + i*2) == -1){
+            perror("pipe");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    int pid;
+    int lastWritePipe = -1;
+    while (command_iterator < len){
+        if (commands[command_iterator][0][0] != '<' && \
+            commands[command_iterator][0][0] != '>' && \
+            commands[command_iterator][0][0] != '|')
+        {
+            pid = execProgram(commands, &command_iterator, len, pd, lastWritePipe);                        
+            if (pid >= 0){
+                if (pids_count + 1> pids_size){
+                    pids_size *= 2;
+                    pids = realloc(pids, pids_size);
+                }
+                lastWritePipe = command_iterator * 2;
+                printf("lastWritePipe value %d\n", lastWritePipe);
+                pids[pids_count++] = pid;
+            } 
+        }  
+        command_iterator++;
+    }
+    
+    //close all pipes;
+    for (int i = 0; i < 2 * len; i++)
+        close(pd[i]);
+
+    if (DEBUG) printf(YELLOW "### Number of process is %d" RESET "\n", pids_count);
+
+    int clid, wstatus;
+    // wait all processes
+    for (int i = 0; i < pids_count; i++)
+    {    
+        clid = wait(&wstatus); // wait for child to finish
+        if (clid == -1) // wait failed
+        {
+            perror("wait failed");
+        }
+        if (WIFEXITED(wstatus)){
+            // child exited normally
+            printf("\nchild exited with status %d\n", WEXITSTATUS(wstatus));
+        }else{
+            printf("\nexited abnormally\n");
+        }
+    }
 }
